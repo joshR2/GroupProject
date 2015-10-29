43e3e14f82522e70a56e3446c4f5ccfbc6185f94
AlexHuntsman <ahun0442@740s-pl-120.local>
2015-10-29 09:41:42 -0600

    Added usability to app

--- app/src/main/java/activity/ctec/groupproject/SendTextActivity.java
+++ app/src/main/java/activity/ctec/groupproject/SendTextActivity.java
@@ -1,17 +1,62 @@
 package activity.ctec.groupproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
+import android.app.Activity;
+import android.widget.Toast;
+import android.telephony.SmsManager;
 
-public class SendTextActivity extends AppCompatActivity {
+
+public class SendTextActivity extends Activity
+{
+    private Button sendButton;
+    private EditText phoneNumber;
+    private EditText messageText;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group);
+
+        sendButton = (Button) findViewById(R.id.sendButton);
+        phoneNumber = (EditText) findViewById(R.id.phoneNumber);
+        messageText = (EditText) findViewById(R.id.messageText);
+
+        setupListeners();
     }
+    private void setupListeners()
+    {
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View currentView) {
+                try {
+                    String contact = phoneNumber.getText().toString();
+                    String message = messageText.getText().toString();
+                    sendSMS(contact, message);
+
+                    Toast.makeText(currentView.getContext(), "message was sent" , Toast.LENGTH_SHORT).show();
+                } catch (Exception currentException) {
+                    Toast.makeText(currentView.getContext(), "message was not sent" , Toast.LENGTH_LONG).show();
+                    Toast.makeText(currentView.getContext(), currentException.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+    /**
+     * Sends a Text based SMS to a specified number.
+     * @param messageAddress the number this app sends a text to.
+     *@param messageContent The message being sent.
+     */
+    private void sendSMS(String messageAddress, String messageContent)
+    {
+        SmsManager mySMSManager = SmsManager.getDefault();
+        mySMSManager.sendTextMessage(messageAddress, null, messageContent,null,null);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -34,4 +79,9 @@ public class SendTextActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
+
 }
+
--- app/src/main/java/activity/ctec/groupproject/SettingsScreen.java
+++
@@ -1,7 +0,0 @@
-package activity.ctec.groupproject;
-
-/**
- * Created by ahun0442 on 10/27/15.
- */
-public class SettingsScreen {
-}
